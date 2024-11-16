29f81bbd4bfa6ea53ac4796254a2863774eb8ec3
sibunamusic <zuercher_tina@bluewin.ch>
2024-11-16 17:29:40 +0100

    Add files via upload

---
+++ types/global.d.ts
@@ -0,0 +1,13 @@
+import { Snowflake } from "discord.js";
+
+declare global {
+    interface String {
+        toSnowflake(): Snowflake;
+    }
+}
+
+declare module "discord.js" {
+    interface EmbedBuilder {
+        toJSON(): Record<string, unknown>;
+    }
+}
